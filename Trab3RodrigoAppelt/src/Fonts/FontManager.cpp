ffe771d43164d7f8d07405942ffaa53d0ccb8f49
Rodrigo Appelt <rappelt@inf.ufsm.br>
2024-04-19 16:20:58 -0300

    custom font manager

--- Trab3RodrigoAppelt/src/Fonts/FontManager.cpp
+++ Trab3RodrigoAppelt/src/Fonts/FontManager.cpp
@@ -1,24 +1,120 @@
 #include "FontManager.h"
-#include "schrift.h"
 
-std::vector<SFT_Font*> FontManager::fonts;
+#include <map>
+#include <tuple>    
 
-void FontManager::load(std::string fontPath)
+#include "../3D/ObjLoader.h"
+
+
+std::map<std::tuple<CustomFont, char>, std::tuple<float,float>> FontManager::glyphSize;
+std::map<std::tuple<CustomFont, char>, std::string> FontManager::objNames;
+
+void FontManager::load(CustomFont font)
 {
-    SFT_Font *font = sft_loadfile(fontPath.c_str());
-    if (font == NULL)
+    switch (font)
     {
-        printf("FontManager::load::Erro ao carregar fonte %s\n", fontPath.c_str());
-        return;
+    case CustomFont::AgencyFB_Digits:
+        // load glyphs
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit0.obj", "agency_digit0");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit1.obj", "agency_digit1");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit2.obj", "agency_digit2");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit3.obj", "agency_digit3");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit4.obj", "agency_digit4");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit5.obj", "agency_digit5");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit6.obj", "agency_digit6");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit7.obj", "agency_digit7");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit8.obj", "agency_digit8");
+        ObjLoader::load("./Trab3RodrigoAppelt/models/agency_digit9.obj", "agency_digit9");
+
+        // calculate glyph sizes
+        for (int i = 0; i < 10; i++)
+        {
+            char c = '0' + i;
+            uint32_t width, height;
+
+            // get max delta in Y of the '1' vertices
+            objNames[std::make_tuple(font, c)] = "agency_digit" + std::to_string(i);
+            ObjFile *obj = ObjLoader::get("agency_digit" + std::to_string(i));
+            float minX = 0, maxX = 0, minY = 0, maxY = 0;
+            for (auto &v : obj->vertices)
+            {
+                if (v.x < minX)
+                {
+                    minX = v.x;
+                }
+                if (v.x > maxX)
+                {
+                    maxX = v.x;
+                }
+                if (v.y < minY)
+                {
+                    minY = v.y;
+                }
+                if (v.y > maxY)
+                {
+                    maxY = v.y;
+                }
+            }
+            width = maxX - minX;
+            height = maxY - minY;
+            glyphSize[std::make_tuple(font, c)] = std::make_tuple(width, height);
+        }
+        break;
+    default:
+        break;
     }
-    fonts.push_back(font);
 }
 
-void FontManager::free()
+void FontManager::getLineHeight(CustomFont font, uint32_t &lineHeight)
 {
-    for (SFT_Font *font : fonts)
+    switch (font)
     {
-        sft_freefont(font);
+    case CustomFont::AgencyFB_Digits:
+        // read '1' height
+        uint32_t width, height;
+        FontManager::getGlyphSize(font, '1', width, height);
+        lineHeight = height;
+        break;
+    default:
+        break;
     }
-    fonts.clear();
+}
+
+void FontManager::getGlyphSize(CustomFont font, char c, uint32_t &width, uint32_t &height){
+    switch (font)
+    {
+    case CustomFont::AgencyFB_Digits:
+        std::tie(width, height) = glyphSize[std::make_tuple(font, c)];
+        break;
+    default:
+        break;
+    }
+
+}
+
+void FontManager::getTextSize(CustomFont font, const std::string &text, uint32_t &width, uint32_t &height){
+    width = 0;
+    height = 0;
+    for (auto &c : text)
+    {
+        uint32_t glyphWidth, glyphHeight;
+        FontManager::getGlyphSize(font, c, glyphWidth, glyphHeight);
+        width += glyphWidth;
+        if (glyphHeight > height)
+        {
+            height = glyphHeight;
+        }
+    }
+}
+
+ObjFile FontManager::getGlyph(CustomFont font, char c){
+    switch (font)
+    {
+    case CustomFont::AgencyFB_Digits:
+        return *ObjLoader::get(objNames[std::make_tuple(font, c)]);
+        break;
+    default:
+        break;
+    }
+    return ObjFile();
 }
--- Trab3RodrigoAppelt/src/Fonts/FontManager.h
+++ Trab3RodrigoAppelt/src/Fonts/FontManager.h
@@ -4,16 +4,41 @@
 #include <string>
 #include <vector>
 
-#include "schrift.h"
+#include "../3D/ObjLoader.h"
 
+enum class CustomFont {
+    AgencyFB_Digits
+};
+
+/// @brief Carrega e gerencia fontes customizadas
+/// usando arquivos obj.
 class FontManager {
 public:
     /// @brief Carrega uma fonte do disco.
-    static void load(std::string fontPath);
-    /// @brief Libera todas as fontes carregadas.
-    static void free();
+    static void load(CustomFont font);
+
+    /// @brief Retorna a altura de uma linha de texto
+    static void getLineHeight(CustomFont font, uint32_t &lineHeight);
+
+    /// @brief Retorna as dimensoes de um caractere em uma fonte
+    static void getGlyphSize(CustomFont font, char c, uint32_t &width, uint32_t &height);
+
+    /// @brief Retorna a largura e altura de um texto
+    /// @param font A fonte a ser usada
+    /// @param text O texto a ser medido
+    /// @param width A largura do texto
+    /// @param height A altura do texto
+    static void getTextSize(CustomFont font, const std::string &text, uint32_t &width, uint32_t &height);
+
+    /// @brief Retorna o arquivo 3d correspondente do caractere 'c' na fonte 'font'
+    /// @param font A fonte a ser usada
+    /// @param c O caractere a ser buscado
+    /// @return O arquivo 3d correspondente
+    static ObjFile getGlyph(CustomFont font, char c);
+
 private:
-    static std::vector<SFT_Font*> fonts;
+    static std::map<std::tuple<CustomFont, char>, std::tuple<float,float>> glyphSize;
+    static std::map<std::tuple<CustomFont, char>, std::string> objNames;
 };
 
 #endif
